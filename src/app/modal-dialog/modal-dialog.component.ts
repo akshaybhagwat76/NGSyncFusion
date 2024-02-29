76173fec450755a9d4ca8c014888eecbbcc85d65
Vinay Khatri <khatrivinay520@gmail.com>
2024-02-29 17:51:18 +0530

    .

--- src/app/modal-dialog/modal-dialog.component.ts
+++ src/app/modal-dialog/modal-dialog.component.ts
@@ -7,20 +7,21 @@ import {
   MatDialogRef,
 } from '@angular/material/dialog';
 
-import {MatButtonModule} from '@angular/material/button';
-import { FormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { AppComponent } from '../app.component';
-import { LinkAnnotationService, BookmarkViewService, MagnificationService,
+import {
+  LinkAnnotationService, BookmarkViewService, MagnificationService,
   ThumbnailViewService, ToolbarService, NavigationService,
   AnnotationService, TextSearchService, TextSelectionService,
   PrintService, FormDesignerService, FormFieldsService,
   PdfViewerModule,
-  PdfViewerComponent, TextFieldSettings, SignatureFieldSettings, InitialFieldSettings, 
+  PdfViewerComponent, TextFieldSettings, SignatureFieldSettings, InitialFieldSettings,
   CheckBoxFieldSettings, RadioButtonFieldSettings
 } from '@syncfusion/ej2-angular-pdfviewer';
-import {data, DataKeys} from "./fakedata";
+import { data, DataKeys } from "./fakedata";
 
 @Component({
   selector: 'app-modal-dialog',
@@ -35,63 +36,61 @@ import {data, DataKeys} from "./fakedata";
     MatDialogActions,
     MatDialogClose,
     AppComponent,
-    PdfViewerModule  ],
-  providers:[LinkAnnotationService, BookmarkViewService, MagnificationService, ThumbnailViewService, ToolbarService,
+    PdfViewerModule],
+  providers: [LinkAnnotationService, BookmarkViewService, MagnificationService, ThumbnailViewService, ToolbarService,
     NavigationService, TextSearchService, TextSelectionService, PrintService, AnnotationService, FormDesignerService, FormFieldsService],
   templateUrl: './modal-dialog.component.html',
   styleUrl: './modal-dialog.component.css'
 })
 export class ModalDialogComponent {
-  
+
   @ViewChild('pdfviewer')
   public pdfviewerControl?: PdfViewerComponent;
   public document: string = 'assets/jfs.pdf';
   public resource: string = "https://cdn.syncfusion.com/ej2/23.1.43/dist/ej2-pdfviewer-lib";
-  
+
   pdfData: any = {};
   constructor(
     public dialogRef: MatDialogRef<ModalDialogComponent>,
-  ) {  
-    
+  ) {
+
   }
-  
+
   loaded() {
-    
+
     this.pdfviewerControl?.enableDownload;
     this.pdfviewerControl?.enableAutoComplete;
     let allFields = this.pdfviewerControl?.retrieveFormFields()
-    if(allFields ){
+    if (allFields) {
       allFields.forEach((field: any, i: number) => {
         if (field.name in data && this.pdfviewerControl) {
           const fieldName = field.name as keyof typeof data;
           const value = data[fieldName];
           if (value !== undefined) {
             this.pdfviewerControl.formDesignerModule.updateFormField(
-                  this.pdfviewerControl.formFieldCollections[i],
-                  { value: value } as TextFieldSettings
-              );
+              this.pdfviewerControl.formFieldCollections[i],
+              { value: value } as TextFieldSettings
+            );
           }
-      }
-      
+        }
       })
+      this.pdfData =allFields;
+      console.log(this.pdfData)
     }
-  
     
-    
-    console.log(JSON.stringify(this.pdfData))
   }
- 
 
-  ngOnInit(){
-    
-    document.querySelectorAll('div').forEach((div)=>{ 
-      if (div.innerText == "This application was built using a trial version of Syncfusion Essential Studio. To remove the license validation message permanently, a valid license key must be included. Claim your free account"){
+
+  ngOnInit() {
+
+    document.querySelectorAll('div').forEach((div) => {
+      if (div.innerText == "This application was built using a trial version of Syncfusion Essential Studio. To remove the license validation message permanently, a valid license key must be included. Claim your free account") {
         div.style.display = "none";
       }
-  })
+    })
   }
- 
- 
+
+
   onNoClick(): void {
     this.dialogRef.close();
   }
