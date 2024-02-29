3adf99ecdd55f53844f0789533010352f01fad2b
Vinay Khatri <khatrivinay520@gmail.com>
2024-02-29 17:31:10 +0530

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalDialogComponent } from './modal-dialog/modal-dialog.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, PdfViewerModule, ModalDialogComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'FreelancerDemo';
+export class AppComponent implements OnInit {
+  title = 'FreelancerDemo ';
+  
+  pdfContent: any = './assets/demo.pdf';
+  editableFields: any = {}; // Object to store editable field values
+
+  ngOnInit():void{
+    this.openDialog()
+  }
+  constructor(public dialog: MatDialog) {}
+
+  openDialog(): void {
+    this.dialog.open(ModalDialogComponent);
+  }
+
 }
---
+++ src/app/modal-dialog/modal-dialog.component.ts
@@ -0,0 +1,98 @@
+import { Component, Inject, ViewChild } from '@angular/core';
+import {
+  MatDialogTitle,
+  MatDialogContent,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogRef,
+} from '@angular/material/dialog';
+
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { AppComponent } from '../app.component';
+import { LinkAnnotationService, BookmarkViewService, MagnificationService,
+  ThumbnailViewService, ToolbarService, NavigationService,
+  AnnotationService, TextSearchService, TextSelectionService,
+  PrintService, FormDesignerService, FormFieldsService,
+  PdfViewerModule,
+  PdfViewerComponent, TextFieldSettings, SignatureFieldSettings, InitialFieldSettings, 
+  CheckBoxFieldSettings, RadioButtonFieldSettings
+} from '@syncfusion/ej2-angular-pdfviewer';
+import {data, DataKeys} from "./fakedata";
+
+@Component({
+  selector: 'app-modal-dialog',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+    AppComponent,
+    PdfViewerModule  ],
+  providers:[LinkAnnotationService, BookmarkViewService, MagnificationService, ThumbnailViewService, ToolbarService,
+    NavigationService, TextSearchService, TextSelectionService, PrintService, AnnotationService, FormDesignerService, FormFieldsService],
+  templateUrl: './modal-dialog.component.html',
+  styleUrl: './modal-dialog.component.css'
+})
+export class ModalDialogComponent {
+  
+  @ViewChild('pdfviewer')
+  public pdfviewerControl?: PdfViewerComponent;
+  public document: string = 'assets/jfs.pdf';
+  public resource: string = "https://cdn.syncfusion.com/ej2/23.1.43/dist/ej2-pdfviewer-lib";
+  
+  pdfData: any = {};
+  constructor(
+    public dialogRef: MatDialogRef<ModalDialogComponent>,
+  ) {  
+    
+  }
+  
+  loaded() {
+    
+    this.pdfviewerControl?.enableDownload;
+    this.pdfviewerControl?.enableAutoComplete;
+    let allFields = this.pdfviewerControl?.retrieveFormFields()
+    if(allFields ){
+      allFields.forEach((field: any, i: number) => {
+        if (field.name in data && this.pdfviewerControl) {
+          const fieldName = field.name as keyof typeof data;
+          const value = data[fieldName];
+          if (value !== undefined) {
+            this.pdfviewerControl.formDesignerModule.updateFormField(
+                  this.pdfviewerControl.formFieldCollections[i],
+                  { value: value } as TextFieldSettings
+              );
+          }
+      }
+      
+      })
+    }
+  
+    
+    
+    console.log(JSON.stringify(this.pdfData))
+  }
+ 
+
+  ngOnInit(){
+    
+    document.querySelectorAll('div').forEach((div)=>{ 
+      if (div.innerText == "This application was built using a trial version of Syncfusion Essential Studio. To remove the license validation message permanently, a valid license key must be included. Claim your free account"){
+        div.style.display = "none";
+      }
+  })
+  }
+ 
+ 
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
